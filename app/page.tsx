d7b20abf465dcfeb034e53f913c2c318bee8c7d0
zhuminghui17 <zhuminghui17@gmail.com>
2025-06-03 18:51:08 -0400

    feat: shadcn.ui set up

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,6 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        {/* Test for Tailwind CSS - Task 1.2 */}
-        <p className="text-red-500 font-bold">✅ Tailwind CSS is working!</p>
-        
         <Image
           className="dark:invert"
           src="/next.svg"
